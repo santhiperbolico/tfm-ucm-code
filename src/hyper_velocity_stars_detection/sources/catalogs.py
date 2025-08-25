f692c4b7abe75db33060b5331345b513daa2edaa
Santiago Arranz <santiago.arranz.sanz@gmail.com>
2025-08-25 22:22:57 +0200

    [IMBH-28] Modificación de laestructurade catálogos
    Se ha modificado la estructura de la descarga y filtrado de datos de los catálogos. Se ha definido el catálogo de GAIA DR3 y queda pendiente revisartests y definir restos de catálogos

--- src/hyper_velocity_stars_detection/sources/catalogs.py
+++ src/hyper_velocity_stars_detection/sources/catalogs.py
@@ -1,11 +1,13 @@
 import logging
+from abc import ABC, abstractmethod
 from typing import Optional
 
 import pandas as pd
 from astroquery.gaia import Gaia
-from attr import attrib, attrs
+from attr import attrs
 
 from hyper_velocity_stars_detection.data_storage import StorageObjectTableVotable
+from hyper_velocity_stars_detection.sources.filter_quality import GAIA_DR3_FIELDS, QueryProcessor
 
 
 class CatalogsType:
@@ -24,49 +26,18 @@ class CatalogError(Exception):
     pass
 
 
-@attrs
-class Catalog:
-    catalog_table = attrib(type=str)
+class Catalog(ABC):
+    catalog_name = "catalog_name"
+    catalog_table = "table_name"
 
-    @classmethod
-    def get_catalog(cls, catalog_name: CatalogsType) -> "Catalog":
-        """
-        Método de clase que dado el nombre del catálogo carga el elemento Catalog
-        con su tabla:
-
-        Parameters
-        ----------
-        catalog_name: CatalogsType
-            Nombre del catálogo
-
-        Returns
-        -------
-        object: Catalog
-            Catálogo instanciado.
-        """
-
-        dic_types = {
-            CatalogsType.GAIA_DR2: CatalogsTables.GAIA_DR2,
-            CatalogsType.GAIA_DR3: CatalogsTables.GAIA_DR3,
-            CatalogsType.GAIA_FPR: CatalogsTables.GAIA_FPR,
-        }
-        try:
-            catalog_table = dic_types[catalog_name]
-        except KeyError:
-            raise CatalogError(f"El catálogo {catalog_name} no existe.")
-        return cls(catalog_table)
-
-    def download_results(
+    def download_data(
         self,
         ra: float,
         dec: float,
         radius: float,
-        output_file: str,
-        filter_parallax_min: Optional[float] = None,
-        filter_parallax_max: Optional[float] = None,
-        filter_parallax_error: Optional[float] = None,
         row_limit: int = -1,
-        return_data: bool = True,
+        output_file: Optional[str] = None,
+        **filter_params,
     ) -> Optional[pd.DataFrame]:
         """
         Método que descarga los datos del catalogo asociado teniendo en cuenta los parámetros.
@@ -79,48 +50,41 @@ class Catalog:
             Declinaje en mas
         radius: float
             Radio de búsqueda en grados.
-        output_file: str
-            Nombre del archivo a guardar.
-        filter_parallax_min: Optional[float], default None
-            Paralaje mínimo que buscar.
-        filter_parallax_max: Optional[float], default None
-            Paralaje máximo que buscar.
-        filter_parallax_error: Optional[float], default None
-            Máximo error en el parale a buscar.
+        output_file: Optional[str] = None,
+            Nombre del archivo donde se quiere guardar los resultados, si se quiere.
         row_limit: int, default -1
             Límite de filas consultadas. Por defecto se extraen todas las filas.
-        return_data: bool, default False
-            Indica si queremos devolver lso datos en forma de dataframe.
+        **filter_params
+            Filtros aplicados a la descarga de datos.
 
         Returns
         -------
         results: pd.DataFrame
             Tabla con los datos del catálogo descargados.
         """
-        Gaia.ROW_LIMIT = row_limit
+        try:
+            self._download_data(
+                ra=ra, dec=dec, radius=radius, row_limit=row_limit, **filter_params
+            )
+        except NotImplementedError:
+            raise NotImplementedError(
+                "No se ha implementado la descarga en el catálogo %s" % self.catalog_name
+            )
 
-        filter = f"""
-        WHERE CONTAINS(
-               POINT('ICRS', ra, dec),
-               CIRCLE('ICRS', {ra}, {dec}, {radius})
-               )=1
+    @abstractmethod
+    def _download_data(
+        self,
+        ra: float,
+        dec: float,
+        radius: float,
+        row_limit: int = -1,
+        output_file: Optional[str] = None,
+        **filter_params,
+    ) -> Optional[pd.DataFrame]:
         """
-        if filter_parallax_max is not None:
-            filter = f"{filter} AND parallax < {filter_parallax_max}"
-        if filter_parallax_min is not None:
-            filter = f"{filter} AND parallax > {filter_parallax_min}"
-        if filter_parallax_error is not None:
-            filter = f"{filter} AND parallax_error < {filter_parallax_error}"
-
-        query = f"SELECT {self.catalog_table}.* FROM {self.catalog_table} {filter}"
-
-        job = Gaia.launch_job_async(
-            query, dump_to_file=True, output_format="votable", output_file=f"{output_file}"
-        )
-        logging.info(job)
-        if return_data:
-            results = job.get_results()
-            return results.to_pandas()
+        Método oculto asociado a download_data.
+        """
+        raise NotImplementedError()
 
     @staticmethod
     def read_catalog(file_catalog: str) -> pd.DataFrame:
@@ -140,3 +104,55 @@ class Catalog:
         """
         results = StorageObjectTableVotable.load(file_catalog).to_pandas()
         return results
+
+
+@attrs
+class GaiaDR3(Catalog):
+    catalog_name = "gaiadr3"
+    catalog_table = "gaiadr3.gaia_source"
+
+    @abstractmethod
+    def _download_data(
+        self,
+        ra: float,
+        dec: float,
+        radius: float,
+        row_limit: int = -1,
+        output_file: Optional[str] = None,
+        **filter_params,
+    ) -> Optional[pd.DataFrame]:
+        """
+        Método que descarga los datos del catalogo asociado teniendo en cuenta los parámetros.
+
+        Parameters
+        ----------
+        ra: float
+           Ascensión recta en mas.
+        dec: float
+           Declinaje en mas
+        radius: float
+           Radio de búsqueda en grados.
+        row_limit: int, default -1
+           Límite de filas consultadas. Por defecto se extraen todas las filas.
+        **filter_params
+            Filtros aplicados a la descarga de datos.
+
+        Returns
+        -------
+        results: pd.DataFrame
+           Tabla con los datos del catálogo descargados.
+        """
+        Gaia.ROW_LIMIT = row_limit
+
+        query_params = {"ra": ra, "dec": dec, "radius": radius}
+        query_params.update(filter_params)
+        query_processor = QueryProcessor(query_params, GAIA_DR3_FIELDS)
+        query = query_processor.get_query(self.catalog_table)
+
+        job = Gaia.launch_job_async(
+            query, dump_to_file=True, output_format="votable", output_file=output_file
+        )
+        logging.info(job)
+
+        results = job.get_results()
+        return results.to_pandas()
